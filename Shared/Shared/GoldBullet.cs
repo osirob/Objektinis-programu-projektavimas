75daf54574e9088a4ce44afec8a737fbdad68ea7
UgniusTerekas <ugniust241@gmail.com>
2022-11-07 17:44:16 +0200

    Decorator bullets made

---
+++ Shared/Shared/GoldBullet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Shared
+{
+    public class GoldBullet : BulletDecorator
+    {
+        public GoldBullet(Bullet bullet) : base(bullet)
+        {
+        }
+
+        public override int CalculateHeight(int standartHeight)
+        {
+            base.CalculateHeight(standartHeight);
+
+            return base.CalculateHeight(standartHeight) + 2;
+        }
+
+        public override int CalculateWidth(int standartWidth)
+        {
+            base.CalculateWidth(standartWidth);
+
+            return base.CalculateWidth(standartWidth) + 2;
+        }
+    }
+}
---
+++ Shared/Shared/RiffleBullet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Shared
+{
+    public class RiffleBullet : Bullet
+    {
+        public string Tag { get; set; }
+        public override int CalculateWidth(int standartWidth)
+        {
+            return standartWidth * 5;
+        }
+
+        public override int CalculateHeight(int standartHeight)
+        {
+            return standartHeight * 5;
+        }
+    }
+}
